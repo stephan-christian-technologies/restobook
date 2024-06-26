a42376d12fe95fde541412ffe5189b0225984928
Stéphan CHRISTIAN <stephan.christian@versusmind.eu>
2024-06-26 19:10:42 +0300

    feat(navigation): added bottom navigation bar

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:restobook_mobile/features/restaurant/presentation/pages/home_page.dart';
+import 'package:restobook_mobile/features/home/presentation/pages/home_page.dart';
+import 'package:restobook_mobile/features/search/presentation/pages/search_page.dart';
+import 'package:restobook_mobile/features/reservation/presentation/pages/reservation_page.dart';
+import 'package:restobook_mobile/features/favorites/presentation/pages/favorites_page.dart';
+import 'package:restobook_mobile/features/profile/presentation/pages/profile_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,8 +13,71 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'RestoBook',
-      home: Container(),
+      title: 'Restaurant App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
+  static final List<Widget> _pages = <Widget>[
+    HomePage(),
+    SearchPage(),
+    ReservationPage(),
+    FavoritesPage(),
+    ProfilePage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _pages.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: 'Reservations',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorites',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        unselectedItemColor: Colors.grey[600],
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
